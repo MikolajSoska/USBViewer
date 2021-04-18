f029cef7f0103b9685878723ca00881dbd9fa1ad
Mikołaj Soska <miki_2007@gazeta.pl>
2021-04-18 21:28:28 +0200

    Added method for getting USB Viewer based on system type.

--- main.py
+++ main.py
@@ -1,8 +1,23 @@
+import platform
+from typing import Optional
+
+from viewer.base import BaseViewer
+from viewer.linux import LinuxViewer
 from viewer.windows import WindowsViewer
 
 
+def get_usb_viewer() -> Optional[BaseViewer]:
+    if platform.system() == 'Windows':
+        return WindowsViewer()
+    elif platform.system() == 'Linux':
+        return LinuxViewer()
+    else:
+        print(f'Script doesn\'t support system: {platform.system()}.')
+        return None
+
+
 def main():
-    viewer = WindowsViewer()
+    viewer = get_usb_viewer()
     print('Getting USB devices saved in system...')
     devices = viewer.get_usb_devices()
     print('Detected devices:')
