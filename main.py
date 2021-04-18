fb920fc6e04783c0606d7bae2c73ffa8b5a881f1
Mikołaj Soska <miki_2007@gazeta.pl>
2021-04-18 22:08:11 +0200

    Fixed imports in main file.

--- main.py
+++ main.py
@@ -2,14 +2,14 @@ import platform
 from typing import Optional
 
 from viewer.base import BaseViewer
-from viewer.linux import LinuxViewer
-from viewer.windows import WindowsViewer
 
 
 def get_usb_viewer() -> Optional[BaseViewer]:
     if platform.system() == 'Windows':
+        from viewer.windows import WindowsViewer  # To prevent from importing 'winreg' in Linux
         return WindowsViewer()
     elif platform.system() == 'Linux':
+        from viewer.linux import LinuxViewer
         return LinuxViewer()
     else:
         print(f'Script doesn\'t support system: {platform.system()}.')
